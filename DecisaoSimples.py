88cddf183dc500cb67b76482c7c818be2a186a98
Lilian Akemi Ofusa Miyamura <61940139+lilianakemi@users.noreply.github.com>
2021-10-12 19:18:54 -0300

    Delete DecisaoSimples.py

--- DecisaoSimples.py
+++
@@ -1,8 +0,0 @@
-nome=input("Digite o nome: ")
-idade=int(input("Digite a idade"))
-if idade>=65: 
-    print("O paciente "+ nome + "tem atendimento prioritario!")
-else:
-    print("O paciente "+ nome + "nao tem atendimento prioritario!")
-
-
